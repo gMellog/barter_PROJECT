a58090970c602abaca54f9460371fd42a8341c10
Aleksandr Khabarov <aekhabarov@gmail.com>
2021-06-04 07:06:26 +0300

    Release 1. Начальная верстка

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,11 @@
-import Searcher from "./components/Searcher/Searcher"
+import Searcher from "./components/Searcher/Searcher";
+import SearchResult from "./components/SearchResult/SearchResult";
 
 function App() {
   return (
     <div className="App">
-      <Searcher/>
+      <Searcher />
+      <SearchResult />
     </div>
   );
 }
