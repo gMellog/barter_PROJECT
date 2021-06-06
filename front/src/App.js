b6c000d746b6bdb1211e1bf2a348fdd8eab0e65f
Aleksandr Khabarov <aekhabarov@gmail.com>
2021-06-06 18:02:33 +0300

    merge errors all solved

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import "./App.css";
 
-import ProfilePanel from "./components/ProfilePanel/ProfilePanel";
-import SearchResult from "./components/SearchResult/SearchResult";
 import NavMenu from "./components/NavMenu/NavMenu";
 import Searcher from "./components/Searcher/Searcher";
 import ShowProducts from "./components/ShowProducts/ShowProducts";
@@ -14,34 +12,23 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import ProfilePanel from "./components/ProfilePanel/ProfilePanel";
 import SearchResult from "./components/SearchResult/SearchResult";
-import React from "react";
-import './App.css';
-import NavMenu from './components/NavMenu/NavMenu'
-import ShowProducts from "./components/ShowProducts/ShowProducts";
-import ProductCard from "./components/ProductCard/ProductCard";
-import Main from "./components/Main/Main";
 
-//Роутеры
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import "./App.css";
+import Main from "./components/Main/Main";
 
 function App() {
   return (
     <div className="App">
       <Router>
-      <NavMenu/>
+        <NavMenu />
         <ProfilePanel />
-      {/* <Searcher/> */}
-      <Switch>
-        <Route exact path="/product/:name" component={ProductCard} />
-        <Route exact path="/" component={ShowProducts} />
-        <Route exact path="/profile" component='' />
-      </Switch>
-    </Router>
+        {/* <Searcher/> */}
+        <Switch>
+          <Route exact path="/product/:name" component={ProductCard} />
+          <Route exact path="/" component={ShowProducts} />
+          <Route exact path="/profile" component="" />
+        </Switch>
+      </Router>
     </div>
   );
 }
