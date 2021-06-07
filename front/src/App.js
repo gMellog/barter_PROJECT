e3d745db675a52cfdf556de177f005119f0a467e
Aleksandr Khabarov <aekhabarov@gmail.com>
2021-06-07 16:24:56 +0300

    YandexMaps added

--- front/src/App.js
+++ front/src/App.js
@@ -24,6 +24,7 @@ function App() {
         <NavMenu />
         <ProfilePanel />
         <AddProduct />
+
         {/* <Switch>
           <Route exact path="/product/:name" component={ProductCard} />
           <Route exact path="/" component={ShowProducts} />
