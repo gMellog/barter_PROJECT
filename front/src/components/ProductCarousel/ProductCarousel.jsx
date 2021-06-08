b5b4c590dc717ad22ac98e184d86ea6788dad140
Aleksey Maslov <amaslov@sharevault.com>
2021-06-08 09:59:31 +0300

    3 components changes

--- front/src/components/ProductCarousel/ProductCarousel.jsx
+++ front/src/components/ProductCarousel/ProductCarousel.jsx
@@ -1,89 +1,48 @@
-import React, {useState} from 'react';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import MobileStepper from '@material-ui/core/MobileStepper';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
-import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import React from "react";
 import { useSelector } from "react-redux";
-import styles from "./ProductCarousel.module.css"
+import styles from "./ProductCarousel.module.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    maxWidth: 1200,
-    flexGrow: 1,
-  },
-  header: {
-    display: 'flex',
-    alignItems: 'center',
-    height: 50,
-    paddingLeft: theme.spacing(4),
-    backgroundColor: theme.palette.background.default,
-  },
-  img: {
-    marginTop: 60,
-    height: 255,
-    maxWidth: 400,
-    overflow: 'hidden',
-    display: 'block',
-    width: '100%',
-  },
- 
-}));
-
-export default function TextMobileStepper() {
+const CarouselProduct = () => {
   const stuffArray = useSelector((state) => state.stuffArray);
-  const carousel = [...stuffArray]
-  const classes = useStyles();
-  const theme = useTheme();
-  const [activeStep, setActiveStep] = useState(0);
-  const maxSteps = stuffArray.length;
-  const countOfShownProduct = 3
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + countOfShownProduct > maxSteps - 1 ? 0 : prevActiveStep + countOfShownProduct);
-  };
-
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - countOfShownProduct < 0 ? maxSteps - 1 : prevActiveStep - countOfShownProduct);
-  };
 
   return (
-    <div className={classes.root}>
-      <div className={styles.wrapper}>{carousel.slice(activeStep, activeStep+countOfShownProduct).map(stuff => {
-      return (
-        <div key={stuff._id}>
-      <Paper square elevation={0} className={classes.header}>
-        <Typography>{stuff.name}</Typography>
-      </Paper>
-      <img
-        className={classes.img}
-        src={stuff.photoUrl}
-        alt={stuff.name}
-      /> 
+    <div className={styles.wrapper_carousel}>
+      <div className={styles.wrapper_carousel_text}>
+        <div>Новые предложения</div>
+        <div>все...</div>
+        <div className={styles.collection_wrapper}>
+        <Carousel
+          swipeable={true}
+          centerMode={true}
+          autoPlay={true}
+          infiniteLoop={true}
+          interval={10000}
+          showArrows={true}
+          emulateTouch={true}
+          showThumbs={false}
+          showStatus={false}
+          swipeScrollTolerance={5}
+          centerSlidePercentage={20}
+          showIndicators={false}
+        >
+          {stuffArray.map((stuff) => {
+            return (<div key={stuff._id} className={styles.item_wrapper}>
+              <img
+                src={stuff.photoUrl[0]}
+                alt="item_icon"
+                className={styles.item_icon}
+              />
+              <div className={styles.item_text}>{stuff.name}</div>
+            </div>)
+          })}
+        </Carousel>
+        </div>
       </div>
-      )
-    })}
-    </div>
-      
-      <MobileStepper
-        steps={Math.floor(maxSteps/countOfShownProduct)+1}
-        position="static"
-        variant="text"
-        activeStep={Math.floor(activeStep/countOfShownProduct)+1}
-        nextButton={
-          <Button size="small" onClick={handleNext}>
-            Next
-            {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
-          </Button>
-        }
-        backButton={
-          <Button size="small" onClick={handleBack}>
-            {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
-            Back
-          </Button>
-        }
-      />
     </div>
   );
-}
+};
+
+export default CarouselProduct;
+
