855097bcc330d541ff5caf91ec45aa065105c596
Aleksandr Khabarov <aekhabarov@gmail.com>
2021-06-10 09:09:38 +0300

    Release 1

--- front/src/components/ProductCard/ProductCard.jsx
+++ front/src/components/ProductCard/ProductCard.jsx
@@ -1,17 +1,38 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import {getAllSearchThunk} from "../../redux/actions/stuffAC"
+import { getAllSearchThunk } from "../../redux/actions/stuffAC";
+import style from "./ProductCard.module.css";
+import Carousel from "react-material-ui-carousel";
+import { Paper, Button } from "@material-ui/core";
 
 export default function ProductCard() {
   const { name } = useParams();
   const stuffArray = useSelector((state) => state.stuffArray);
-
+  console.log(stuffArray);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getAllSearchThunk(name));
   }, [name]);
+  //Нужно при получении массива с картинками создать массив объектов с полем src и адресом до картинки
+  // const items = arrOfImg.map(el => {src: el})
+  var items = [
+    {
+      src: "https://sobakevi4.ru/wp-content/uploads/2020/10/shelti_01.jpg",
+    },
+    {
+      src: "http://forumimage.ru/uploads/20200517/15897013366079418.jpg",
+    },
+  ];
+
+  function Item(props) {
+    return (
+      <Paper>
+        <img src={props.item.src} />
+      </Paper>
+    );
+  }
 
   return (
     // <div>
@@ -21,6 +42,28 @@ export default function ProductCard() {
     //     </div>
     //   ))}
     // </div>
-    
+    stuffArray.map((stuff) => {
+      return (
+        <div className={style.card_wrapper}>
+          <div className={style.card_title}>{/* {product.title} */}Product Title</div>
+          <div className={style.card_mainImg}>
+            <Carousel autoPlay={false}>
+              {items.map((item, i) => (
+                <Item key={i} item={item} />
+              ))}
+            </Carousel>
+          </div>
+          <div className={style.card_describtion}>
+            <p>
+              Выглядят как настоящий Паль-йот. Однако совсем не работают :(
+              Думаю можно починить рублей за 500.
+            </p>
+            {/* <p>{product.describtion}</p> */}
+          </div>
+          {/* Только для зареганых пользователей */}
+          <div className={style.control_area}>Предложить обмен</div>
+        </div>
+      );
+    })
   );
 }
